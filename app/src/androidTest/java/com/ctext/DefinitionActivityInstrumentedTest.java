3f4dada4063e5ee584886a8147a758a2ab4609a6
DPigeon <D_Pig@encs.concordia.ca>
2020-06-20 18:08:08 -0400

    CICD fixes

--- app/src/androidTest/java/com/ctext/DefinitionActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/ctext/DefinitionActivityInstrumentedTest.java
@@ -17,11 +17,9 @@ import androidx.test.rule.ActivityTestRule;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
